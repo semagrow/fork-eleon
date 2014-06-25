4dafc5b03406932c483dc56cd245316753d5dd78
Giannis Mouchakis <gmouchakis@iit.demokritos.gr>
2014-06-25 15:20:51 +0300

    removed internall loading of extra vocabularies. will implement another way to handle those.

--- src/gr/demokritos/iit/eleon/MainShell.java
+++ src/gr/demokritos/iit/eleon/MainShell.java
@@ -675,18 +675,20 @@ public class MainShell extends Shell
 					//FIXME: do all auto-filled facets
 					OntModel schema = AnnotationVocabulary.getNewModel( AnnotationVocabulary.NONE );
 					java.util.List<DataSchema> dataSchemaList = new ArrayList<DataSchema>();
-					boolean loaded_dc = false;
+					//boolean loaded_dc = false;
 					for (String selectedVocabulary : selectedVocabularies) {
 						for (MenuItem menuItem : dataSchemaMenu.getItems()) {
 							if (menuItem.getSelection()) {
 								if (selectedVocabulary.equals(menuItem.getText())) {
 									//schema.read("file:////" + ((DataSchema) menuItem.getData()).getSchemaFile().getAbsolutePath());
 									try {
-										if ( ! loaded_dc ) {
-											//schema.read(schema_folder + "dcterms.rdf");
+										/*if ( ! loaded_dc ) {
+											schema.read(schema_folder + "dcterms.rdf");
 											schema.read(schema_folder + "dcelements.rdf");
+											//schema.read(schema_folder + "cidoc_crm_v5.0.4_official_release.rdfs");
+											schema.read(schema_folder + "crm.rdf");
 											loaded_dc = true;
-										}
+										}*/
 										String path = ((DataSchema) menuItem.getData()).getSchemaFile().toURI().toURL().toString();
 										String decoded_path = URLDecoder.decode(path, "UTF-8");
 										schema.read(decoded_path);
